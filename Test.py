81850ad6721e0ab71c9d93f26847518ce9ca12e0
BFGm <Timaa32@gmail.com>
2021-10-08 00:57:39 +0300

    Classes works fine, finally can get button/window/label etc states in functions without 9999 function's args and get the result back to classes. So, unstaked. Still don't understand why do i really need in class parent's class init. Tomorrow will start finally filling first table in database. Huh.

--- Test.py
+++ Test.py
@@ -2,10 +2,11 @@ from tkinter import *
 import tkinter.ttk as tk
 from psycopg2 import *
 
+global conn, status_now
 conn = ()
+status_now = "Not connected ..."
 
 class basic(Widget):
-
     def __init__(self, parent, w_type, **sets):
         self.parent = parent
         self.status = 0
@@ -13,7 +14,6 @@ class basic(Widget):
         self.new_wid = self.type(parent)
         for a, b in sets.items():
             self.new_wid[a] = b
-       
     def grid(self, col = 0, r = 0, cols = 1, rows = 1, stick = ""):
         if self.status == 2:
             self.new_wid.grid()
@@ -21,17 +21,30 @@ class basic(Widget):
             return
         self.new_wid.grid(column = col, row = r, columnspan = cols, rowspan = rows, sticky = stick)
         self.status = 1
-
     def remove(self):
         self.new_wid.grid_remove()
         self.status = 2
-
     def upd(self, **sets):
         for a, b in sets.items():
-            self.mew_wid.config(a = b)
+            self.new_wid[a] = b
+
+#class entry_w(Entry):
+#    def __init__(self, parent, w_type, **sets):
+#        super(entry_w, self).__init__()
+#        self.parent = parent
+#        self.status = 0
+#        self.type = w_type
+#        self.new_wid = self.type(parent)
+#        for a, b in sets.items():
+#            self.new_wid[a] = b
+#    def grid(self, col = 0, r = 0, cols = 1, rows = 1, stick = ""):
+#        basic.grid(self, col, r, cols, rows, stick = "")
+#    def remove(self):
+#        basic.remove(self)
+#    def upd(self, **sets):
+#        basic.upd(self, **sets)
 
 class trees(tk.Treeview):
-
     def __init__(self, parent, **sets):
         self.parent = parent
         self.status = 0
@@ -40,7 +53,6 @@ class trees(tk.Treeview):
             self.tree[a] = b
         self.scroll = tk.Skrollbar(parent, command = self.tree.yview)
         self.tree.configure(yscrollcommand = self.scroll.set)
-
     def grid(self, col = 0, r = 0, cols = 1, rows = 1, stick = ""):
         if self.status == 2:
             self.tree.grid()
@@ -50,7 +62,6 @@ class trees(tk.Treeview):
         self.tree.grid(column = col, row = r, columnspan = cols, rowspan = rows, sticky = stick)
         self.scroll.grid(column = col + 1, row = r, columnspan = 1, rowspan = rows, sticky = N + S)
         self.status = 1
-
     def size(self, col_n, **sets):
         a = 0
         while a <= col_n:
@@ -58,13 +69,11 @@ class trees(tk.Treeview):
                 self.tree.column("#{}".format(a), minwidth = sets.get("minwidth_{}".format(a), 0), width = sets.get("width_{}".format(a), 100), \
                         stretch = sets.get("stretch_{}".format(a,), "YES"))
                 a += 1
-
     def remove(self):
         self.tree.grid_remove()
         self.status = -1
 
 class wind(Tk):
-
     def __init__(self, parent = None, **sets):
         Toplevel.__init__(self, parent)
         self.title(sets.get('text', ""))
@@ -72,24 +81,20 @@ class wind(Tk):
         self.status = 0
         self.resizable(0, 0)
         self.protocol("WM_DELETE_WINDOW", sets.get("close", self.closing))
-    
     def closing(self):
         self.status = 0
         self.withdraw()
-
     def open(self):
         if self.status == 1:
             return
         self.status = 1
         self.deiconify()
-
     def table(self, *sets):
         test = 0
         cr = 0
         num = 0
         ms = 0
         for a in sets:
-            print(a)
             if test == 0:
                 test = 1
                 cr = a
@@ -100,27 +105,70 @@ class wind(Tk):
                 ms = a
                 if cr == 'r':
                     self.rowconfigure(num, minsize = ms)
-                    print("row", num,ms)
                 if cr == 'c':
                     self.columnconfigure(num, minsize = ms)
-                    print("col",num,ms)
                 test = 0
 
+class programm:
+    def __init__(self, root):
+        root.title("База данных цен")
+        root.geometry('590x220+{}+{}'.format((root.winfo_screenwidth() // 2 - 300), (root.winfo_screenheight() // 2 - 100)))
+        #root.protocol("WN_DELETE_WINDOW")
+
+        self.mat_w = wind(root, text = "Материалы")
+        self.box_w = wind(root, text = "Корпуса")
+        self.sch_w = wind(root, text = "Схемы")
+        self.auth_w = auth_w(root, text = "Авторизация", close = lambda: root.destroy())
+        self.sett_w = wind(root,text = "Настройки")
+
+        wind.table(root, "r", 0, 20, "r", 1, 30, "r", 2, 20, "c", 0, 20, "c", 1, 50, "c", 2, 20, "c", 3, 50, "c", 4, 20, "c", 5, 50, "c", 6, 20)
+
+        self.mat_w_open = basic(root, Button, text = "Материалы", command = lambda: self.auth_w.open(), width = 10)
+        self.mat_w_open.grid(1, 1, 1, 1, N + S + W + E)
+        self.box_w_open = basic(root, Button, text = "Корпуса", command = lambda: self.box_w.open(), width = 10)
+        self.box_w_open.grid(3, 1, 1, 1, N + S + W + E)
+        self.sch_w_open = basic(root, Button, text = "Схемы", command = lambda: self.sch_w.open(), width = 10)
+        self.sch_w_open.grid(5, 1, 1, 1, N + S + W + E)
+        self.setts = basic(root, Button, bitmap = 'gray12', command = lambda: self.sett_w.open())
+        self.setts.grid(0, 3, 1, 1, N + S + W + E)
+        self.status = basic(root, Label, text = status_now, bg = 'grey')
+        self.status.grid(1, 3, 7, 1, N + S + W + E)
+
+class auth_w(wind):
+    def __init__(self, parent, **sets):
+        self.auth_w = wind.__init__(self, parent, **sets)
+        
+        self.protocol("WM_DELETE_WINDOW", lambda: main_w.destroy())
+        self.status = "Введите логин/пароль"
+        self.note = basic(self, Label, text = self.status)
+        self.entry_log = basic(self, Entry, width = 30)
+        self.entry_pass = basic(self, Entry, width = 30, show = "*")
+        self.butt_ok = basic(self, Button, text = "Ок", width = 8, command = lambda: self.try_auth())
+        self.butt_cancel = basic(self, Button, text = "Отмена", width = 8, command = lambda: main_w.destroy())
+        self.note.grid(1, 1, 3, 1, N + S + W + E)
+        self.entry_log.grid(1, 2, 3, 1, N + S + W + E)
+        self.entry_pass.grid(1, 4, 3, 1, N + S + W + E)
+        self.butt_ok.grid(1, 6, 1, 1, N + S + W + E)
+        self.butt_cancel.grid(3, 6, 1, 1, N + S + W + E)
+        self.table('r', 0, 20, 'r', 3, 10, 'r', 5, 20, 'r', 7, 20, 'c', 0, 20, 'c', 4, 20)
+
+    def try_auth(self):
+        global conn, status_now
+        self.login = self.entry_log.new_wid.get()
+        self.passw = self.entry_pass.new_wid.get()
+        try:
+            conn = connect(dbname = "Prices", user = self.login, password = self.passw, port = "5432")
+            status_now = "Connected"
+            main.status.upd(text = status_now)
+            self.destroy()
+        except:
+            self.status = "Неверный логин/пароль"
+            self.note.upd(text = self.status)
+            
+
+#def connection():
+#    global conn, status
+
 main_w = Tk()
-main_w.title("База данных цен")
-main_w.geometry('590x220+{}+{}'.format((main_w.winfo_screenwidth() // 2 - 300), (main_w.winfo_screenheight() // 2 - 100)))
-main_w.protocol("WN_DELETE_WINDOW")
-
-mat_w = wind(main_w, text = "Материалы", )
-box_w = wind(main_w, text = "Корпуса")
-sch_w = wind(main_w, text = "Схемы")
-mat_w.table("r", 0 , 20, "r", 3, 39, "r", 5, 20, 'c', 0, 20, 'c', 2, 20, 'c', 4, 20, 'c', 6, 20, 'c', 8, 20, 'c', 10, 200)
-
-main_mat_w_open = basic(main_w, Button, text = "Материалы", command = lambda: mat_w.open())
-main_mat_w_open.grid(1, 1, 1, 1, N + S + W + E)
-main_box_w_open = basic(mat_w, Button, text = "Корпуса", command = lambda:box_w.open())
-main_box_w_open.grid(1, 1, 1, 1, N + S + W + E)
-main_sch_w_open = basic(main_w, Button, text = "Схемы", command = lambda: sch_w.open())
-main_sch_w_open.grid(5, 1, 1, 1, N + S + W + E)
-
+main = programm(main_w)
 main_w.mainloop()
